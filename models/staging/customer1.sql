6d2f27d69fcaf040e1dab9aa340de8074a40f896
suraj sharma <surajdhn987@gmail.com>
2026-04-19 00:56:44 +0000

    commit

---
+++ models/staging/customer1.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+
+WITH tb1 as(
+    select
+    id  as cust_id,
+    first_name,
+    last_name
+    from {{source('datafeed_shared_schema','raw_customerdata')}})
+select * from tb1
