fc2fe44240a5aa27fcf205bd2f4e4d62c0a8699d
suraj sharma <surajdhn987@gmail.com>
2026-04-19 02:04:38 +0000

    commit

--- models/staging/customer1.sql
+++ models/staging/customer1.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
 WITH tb1 as(
     select
--- models/staging/empheral.sql
+++ models/staging/empheral.sql
@@ -1,9 +1,9 @@
-{{ config(materialized='table',transient='false') }}
+{{ config(materialized='ephemeral') }}
 
 WITH tb1 as(
     select id,
     order_date,
     user_id,
-    
+    status
     from {{source('datafeed_shared_schema','raw_orders')}})
 select * from tb1
